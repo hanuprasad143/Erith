7478910146db257abdc7cdb6fe30d62174253547
prasad <durgaprasaddpprasad@gmail.com>
2025-10-31 19:22:32 +0530

    comment to import sections to page.tsx page

--- src/app/(home)/(home-main)/page.tsx
+++ src/app/(home)/(home-main)/page.tsx
@@ -2,27 +2,27 @@ import React from "react";
 import { Metadata } from "next";
 import HeroAreaOne from "@/components/hero-area/hero-area-one";
 import ServiceOne from "@/components/service/service-one";
-import AboutOne from "@/components/about/about-one";
-import CounterOne from "@/components/counter/counter-one";
-import ProgramArea from "@/components/program/program-area";
+// import AboutOne from "@/components/about/about-one";
+// import CounterOne from "@/components/counter/counter-one";
+// import ProgramArea from "@/components/program/program-area";
 import EventArea from "@/components/event/event-area";
-import TestimonialOne from "@/components/testimonial/testimonial-one";
-import BlogOne from "@/components/blog/blog-one";
-import InstagramArea from "@/components/instagram/instagram-area";
-import CtaOne from "@/components/cta/cta-one";
-import TeamArea from "@/components/team/team-area";
-import LeadershipArea from "@/app/(university)/university-leadership/_components/leadership-area";
-import TestimonialTwo from "@/components/testimonial/testimonial-two";
-import HeaderTopSection from "@/components/hero-area/header-top-section";
+// import TestimonialOne from "@/components/testimonial/testimonial-one";
+// import BlogOne from "@/components/blog/blog-one";
+// import InstagramArea from "@/components/instagram/instagram-area";
+// import CtaOne from "@/components/cta/cta-one";
+// import TeamArea from "@/components/team/team-area";
+// import LeadershipArea from "@/app/(university)/university-leadership/_components/leadership-area";
+// import TestimonialTwo from "@/components/testimonial/testimonial-two";
+// import HeaderTopSection from "@/components/hero-area/header-top-section";
 import ServicesStyle1 from "@/components/services/services-style1";
 import AboutStyle1 from "@/components/about/about-erith";
 import RequestCallStyle1 from "@/components/request/requeststyle";
 import ContactForm from "@/components/form/contact-form";
 import PricingPage from "@/components/globe/erithWorld";
-import Features from "@/components/feature/features"
-import HeroSlider from "@/components/hero-area/hero-slider";
-import BackToTop from "@/components/back-to-top";
-import HeroAreaFive from "@/components/hero-area/hero-area-five";
+// import Features from "@/components/feature/features"
+// import HeroSlider from "@/components/hero-area/hero-slider";
+// import BackToTop from "@/components/back-to-top";
+// import HeroAreaFive from "@/components/hero-area/hero-area-five";
 
 export const metadata: Metadata = {
   title: "Erith Global",
